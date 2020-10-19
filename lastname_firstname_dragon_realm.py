80f0b6d1612d43c85388f7d99ca79bcc718ce70b
Ryan Kelley <kelleyr@duvalschools.org>
2020-10-19 10:27:13 -0400

    Create lastname_firstname_dragon_realm.py

---
+++ lastname_firstname_dragon_realm.py
@@ -0,0 +1,43 @@
+# <Program Name>, <Author Name>, <Date> <Time>, <Version>
+
+# Import the library for random numbers. 
+# Import the library for time.
+
+# Create our first function to display an intro.
+# The intro will be a multi-line comment.
+def display_intro():
+    print("""You will need to write a paragraph description as an introduction.\n""")
+
+def choose_cave():
+    # Declare a variable named cave, initialize it to an EMPTY STRING.
+    # Create a while loop that will as long as cave is NOT equal to 1 and 2.
+        # Use input() to allow the player to choose between cave 1 or 2.
+
+    # Return the cave variable.
+
+# Define a function named check_cave that takes chosen_cave as an argument.
+    # print() a string that describes approaching the cave.
+    # Sleep for 2-3 seconds.
+    # print() a string that describes being just outside the cave.
+    # Sleep for 2-3 seconds.
+    # print() a string that describes seeing the dragon.
+    # Sleep for 2-3 seconds.
+    # print() a string that describes the dragon noticing / seeing you.
+    # Sleep for 2-3 seconds.
+
+    # Declare a variable called friendly_cave and randomly assign it a value of 1 or 2.
+
+    # Check if the chosen_cave IS EQUAL to the friendly_cave.  Make sure to convert friendly_cave into a str().
+        # If it is, print a friendly message from the dragon.
+    # Else, the player the chose the bad cave.
+        # Print a message that dragon kills and eats the player. 
+
+# Version Numbers
+# v0.0 is all of the program information.
+# v0.1 will import the two required libraries.
+# v0.2 completes the function to display the intro. 
+# v0.3 completes the function to choose a cave.
+# v0.4 completes the completes the print statements for the check_cave function.
+# v0.5 declares and assigns the friendly_cave variable.
+# v0.6 completes the check for friendly vs. non-friendly dragon in check_cave.
+
